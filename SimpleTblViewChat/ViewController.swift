bc3b5529773cd0946f08fd221fa728aac465155b
Singh-Mohit <mohitsinghmxp@gmail.com>
2021-02-21 16:46:58 +0530

    upside down tblview

--- SimpleTblViewChat/ViewController.swift
+++ SimpleTblViewChat/ViewController.swift
@@ -8,21 +8,55 @@
 import UIKit
 import LoremIpsum
 
+//MARK: - RAJA's Method
 class ViewController: UIViewController {
 
     // MARK: - IBOutlets
     @IBOutlet weak var tblViewChat: UITableView!
     @IBOutlet weak var txfMsgInput: UITextField!
     @IBOutlet weak var btnSend: UIButton!
+    @IBOutlet weak var txfMsgInputBCons: NSLayoutConstraint!
     
     // MARK: - Properties
-    var sampleData = ["Hope"]
-
+    var sampleData = ["0 Hope"]
+    var keyboardDismiss = true
+    
+    private(set) var keyboardIsOpen: Bool = false
+    private(set) var keyboardFrame: CGRect = .zero
+    
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        tblViewChat.transform = CGAffineTransform(scaleX: 1, y: -1)
     }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let keyboardFrame =  (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+            let isKeyboardShowing = notification.name == UIResponder.keyboardWillShowNotification
+            txfMsgInputBCons.constant = isKeyboardShowing ? keyboardFrame.height : 20
+            UIView.animate(withDuration: 0, delay: 0, options: UIView.AnimationOptions.curveEaseOut, animations: {
+                self.view.layoutIfNeeded()
+            }) { (_) in
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let keyboardFrame =  (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+            let isKeyboardShowing = notification.name == UIResponder.keyboardDidHideNotification
+            txfMsgInputBCons.constant = isKeyboardShowing ? keyboardFrame.height : 20
+            UIView.animate(withDuration: 0, delay: 0, options: UIView.AnimationOptions.curveEaseOut, animations: {
+                self.view.layoutIfNeeded()
+            }) { (_) in
+            }
+        }
+    }
+
+    
     
     // MARK: - Action Methods
     @IBAction func btnActionSend(_ sender: UIButton) {
@@ -31,18 +65,20 @@ class ViewController: UIViewController {
         case 1:
             let numOfWords: UInt = UInt([1, 2, 3].randomElement()!)
             let words: String = LoremIpsum.words(withNumber: numOfWords)
-            sampleData.append(words)
+            sampleData.insert("\(self.sampleData.count) " + words, at: 0)
+        
         case 2:
             let numOfSen: UInt = UInt([2, 3, 4].randomElement()!)
             let sentences = LoremIpsum.sentences(withNumber: numOfSen)
-            sampleData.append(sentences)
+            sampleData.insert("\(self.sampleData.count) " + sentences, at: 0)
+        
         case .none:
             break
+        
         case .some(_):
             break
         }
-        tblViewChat.insertRows(at: [IndexPath(row: self.sampleData.count - 1, section: 0)], with: .fade)
-        tblViewChat.scrollToRow(at: IndexPath(row: self.sampleData.count - 1, section: 0), at: .bottom, animated: true)
+        tblViewChat.insertRows(at: [IndexPath(row: 0, section: 0)], with: .top)
     }
 }
 
@@ -55,8 +91,18 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChatTxtTblCell", for: indexPath) as! ChatTxtTblCell
+        cell.contentView.transform = CGAffineTransform(scaleX: 1, y: -1)
         cell.lblTxt.text = sampleData[indexPath.row]
         return cell
     }
 }
 
+//
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let translation = scrollView.panGestureRecognizer.translation(in: scrollView.superview)
+        if txfMsgInput.isFirstResponder && translation.y > 20 {
+            txfMsgInput.resignFirstResponder()
+        }
+    }
+}
